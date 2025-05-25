e72848a2929b5db38d6b74b490fd6f983569bfd3
Munther Basyouni <munther.basuni@gmail.com>
2025-05-26 02:40:51 +0400

    (add): default homepage to dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+      },
+    ]
+  },
 }
 
 export default nextConfig
